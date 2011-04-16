1dee117f28a9522cf3ec5b56a245702788c7f683
Michel Legnered <michel.legnered@gmail.com>
2011-04-16 15:47:19 +0200

    code refactoring, dictionary d:

--- server.js
+++ server.js
@@ -26,72 +26,70 @@ var o = {
 http.createServer(function(req, res) {
 	var uri = url.parse(req.url).pathname;
 
-	switch (uri) {
-	case '/':
-		var query = url.parse(req.url, true).query;
+	// Static files
+	if(/static/i.test(uri))
+	{
+		var filename = path.join(process.cwd(), uri);
 
-		if (query && query.q)
-			search(req, res, query.q);
-		else
-			renderHtml(res, 'index.html');
-		break;
-	case '/upload':
-		upload(req, res);
-		break;
-	case '/approve':
-		approve(req, res);
-		break;
-	default:
-		// Test for static files
-		//
-		if(/static/i.test(uri))
-		{
-			var filename = path.join(process.cwd(), uri);
-
-			path.exists(filename, function(exists) {
-				if(!exists) {
-					res.writeHead(404);
-					res.end();
-					return;
+		path.exists(filename, function(exists) {
+			if(!exists) {
+				res.writeHead(404);
+				res.end();
+				return;
+			}
+			
+			fs.readFile(filename, "binary", function(err, file) {
+				if(err) {
+					res.writeHead(500, {"Content-Type": "text/plain"});
+					res.end(err + "\n");
 				}
-				
-				fs.readFile(filename, "binary", function(err, file) {
-					if(err) {
-						res.writeHead(500, {"Content-Type": "text/plain"});
-						res.end(err + "\n");
-					}
 
-					res.writeHead(200);
-					res.end(file, "binary");
-				});
+				res.writeHead(200);
+				res.end(file, "binary");
 			});
-		}
-		// Else we have an item to display
-		//
-		else {
+		});
+	} else {
+		// URI routes
+		switch(uri) {
+		case '/':
+			var query = url.parse(req.url, true).query;
+
+			if (query && query.q)
+				search(req, res, query.q);
+			else
+				renderHtml(res, 'index.html');
+			break;
+		case '/upload': upload(req, res); break;
+		case '/approve': approve(req, res); break;
+		default:
+			// Display item
 			var item_id = uri.slice(1);
-			console.log(item_id);
 			redis.hget('i:'+item_id, 'info', function (err, results) {
 				if (results)
-					renderHtml(res, 'item.html', {key: item_id, info: nl2br(results), title: results.substring(0, 30)});
+					renderHtml(res, 'item.html', {
+						key: item_id,
+						info: nl2br(results),
+						title: results.substring(0, 30)
+					});
 				else {
 					res.writeHead(404);
 					res.end();
 				}
-					
 			});
 		}
-		break;
 	}
-
 }).listen(8080);
 
 sys.puts("Server running at http://localhost:8080/");
 
 function search (req, res, query) {
 	var
-	words = query.toLowerCase().split(' '),	
+	words = query.toLowerCase().split(' '),
 	output = { query: query, items: [] };
+
+	// Prefix dictionary to words.
+	for (var x in words)
+		words[x] = 'd:'+words[x];
 
 	redis.sinter(words, function(err, item_ids) {
 		// http://stackoverflow.com/questions/4288759/asynchronous-for-cycle-in-javascript
@@ -113,127 +111,6 @@ function search (req, res, query) {
 					  renderHtml(res, 'index.html', output);
 				  });
 	});	
-}
-
-function approve (req, res) {
-	if (req.method == 'POST') {
-		var
-		form = new formidable.IncomingForm(),
-		email_pattern =  /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-
-		form.parse(req, function(err, fields) {
-			
-			if (fields.approve_items && fields.upload_session_id && fields.uploader_email.match(email_pattern)) {
-				var special_key = Math.uuid(16);
-
-				console.log(fields);
-
-				var server = email.server.connect(o.smtp_data);
-				
-				redis.hset(special_key, 'upload_session_id', fields.upload_session_id, function(err, results) {
-					redis.hset(special_key, 'uploader_email', fields.uploader_email, function(err, results) {
-						redis.expire(special_key, o.approve_ttl);
-						
-						bind.toFile(o.templates_folder+'/email_sent.html', {special_key: special_key}, function callback(data) {
-							res.writeHead(200, {
-								'Set-Cookie': 'uploadSessionId='+
-									fields.upload_session_id+'; expires=Thu, 01-Jan-1970 00:00:01 GMT;',
-								'Content-Type': 'text/html'
-							});
-							res.end(data);
-						});
-					});
-				});				
-			} else if (fields.clear_items && fields.upload_session_id) {
-				redis.lrange(fields.upload_session_id, 0, -1, function (err, item_ids) {
-					if (!err) {
-						item_ids.push(fields.upload_session_id);
-						redis.del(item_ids, function (err, results) {});
-					}
-					bind.toFile(o.templates_folder+'/redirect.html', { location: '/upload' }, function callback(data) {
-						res.writeHead(200, {
-							'Set-Cookie': 'uploadSessionId='+
-								fields.upload_session_id+'; expires=Thu, 01-Jan-1970 00:00:01 GMT;',
-							'Content-Type': 'text/html'
-						});
-						res.end(data);
-					});
-				});
-			} else {
-				res.writeHead(302, {
-					Location: '/upload'
-				});
-				res.end();
-			}
-		});
-	}
-
-	if (req.method == 'GET') {
-		var
-		query = url.parse(req.url, true).query,
-		output = {
-				items: [],
-				count: 0,
-		};
-
-		if (query && query.k) {
-			// Get upload session id from special key
-			redis.hgetall(query.k, function (err, session_data) {
-				if (!err && session_data.upload_session_id) {
-
-					// Return all uploaded item ids
-					redis.lrange(session_data.upload_session_id, 0, -1, function (err, item_ids) {
-						// Delete special key with session data and session data list.
-						redis.del([query.k, session_data.upload_session]);
-
-						// For each item, return item data
-						item_ids.forEach(function (item_id) {
-							redis.hget('i:'+item_id, 'info', function (err, item_info) {
-								///// Add item id (key) to word sets
-								//
-								// This might be an ugly hack. Find a better wat to eliminate
-								// whitespaces, line return etc. without adding empty strings
-								// to the words_arr. However, only if there's speed or memory gains.
-								var words = item_info.replace(/[^\wåäöÅÄÖ\s]/g, '');
-								words = words.replace(/[\s]/g, ',')
-								var words_arr = words.split(',');
-								console.log('Array with words: '+words_arr);
-								
-								for (x in words_arr) {
-									// How important words of char length < 3 can there be in the
-									// dictionary of humanity?
-									if (words_arr[x].length > 2) {
-										console.log('Adding word to dictionary: '+words_arr[x]);
-										redis.sadd(words_arr[x].toLowerCase(), item_id);
-									}
-								}
-
-								// Add email and remove TTL
-								redis.hset('i:'+item_id, 'email', session_data.uploader_email);
-
-								// Add item to user set
-								redis.sadd(session_data.uploader_email, item_id);
-
-								output.items.push({
-									key: item_id,
-									info: item_info.substring(0, 70)
-								});
-								output.count++;
-								
-								renderHtml(res, 'approved.html', output);
-							});
-						});
-					});
-				} else {
-					res.writeHead(404);
-					res.end();
-				}
-			});
-		} else {
-			res.writeHead(404);					
-			res.end();
-		}
-	}
 }
 
 function upload (req, res) {
@@ -326,6 +203,136 @@ function upload (req, res) {
 		} else
 			renderHtml(res, 'upload.html');
 	});
+}
+
+function approve (req, res) {
+	if (req.method == 'POST') {
+		var
+		form = new formidable.IncomingForm(),
+		email_pattern =  /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+		form.parse(req, function(err, fields) {
+			// Generate special key and send approval email
+			//
+			if (fields.approve_items && fields.upload_session_id && fields.uploader_email.match(email_pattern)) {
+				var
+				special_key = Math.uuid(16);
+				//var server = email.server.connect(o.smtp_data);
+
+				// Only redis and email sent to user will know the value of
+				// our special key.
+				//
+				redis.hset(special_key, 'upload_session_id', fields.upload_session_id, function(err, results) {
+					redis.hset(special_key, 'uploader_email', fields.uploader_email, function(err, results) {
+						redis.expire(special_key, o.approve_ttl);
+						
+						bind.toFile(o.templates_folder+'/email_sent.html', {special_key: special_key}, function callback(data) {
+							res.writeHead(200, {
+								'Set-Cookie': 'uploadSessionId='+
+									fields.upload_session_id+'; expires=Thu, 01-Jan-1970 00:00:01 GMT;',
+								'Content-Type': 'text/html'
+							});
+							res.end(data);
+						});
+					});
+				});				
+			}
+			// Clear items
+			//
+			else if (fields.clear_items && fields.upload_session_id) {
+				redis.lrange(fields.upload_session_id, 0, -1, function (err, item_ids) {
+					if (!err) {
+						item_ids.push(fields.upload_session_id);
+						redis.del(item_ids, function (err, results) {});
+					}
+					bind.toFile(o.templates_folder+'/redirect.html', { location: '/upload' }, function callback(data) {
+						res.writeHead(200, {
+							'Set-Cookie': 'uploadSessionId='+
+								fields.upload_session_id+'; expires=Thu, 01-Jan-1970 00:00:01 GMT;',
+							'Content-Type': 'text/html'
+						});
+						res.end(data);
+					});
+				});
+			}
+			// Do nothing if user hasn't filled in fields correctly, or if
+			// there aren't anything to clear and so on...
+			//
+			else {
+				res.writeHead(302, {
+					Location: '/upload'
+				});
+				res.end();
+			}
+		});
+	}
+
+	if (req.method == 'GET') {
+		var
+		query = url.parse(req.url, true).query,
+		output = {
+				items: [],
+				count: 0,
+		};
+
+		if (query && query.k) {
+			// Get upload session id from special key
+			redis.hgetall(query.k, function (err, session_data) {
+				if (!err && session_data.upload_session_id) {
+
+					// Return all uploaded item ids
+					redis.lrange(session_data.upload_session_id, 0, -1, function (err, item_ids) {
+						// Delete special key with session data and session data list.
+						redis.del([query.k, session_data.upload_session]);
+
+						// For each item, return item data
+						item_ids.forEach(function (item_id) {
+							redis.hget('i:'+item_id, 'info', function (err, item_info) {
+								///// Add item id (key) to word sets
+								//
+								// This might be an ugly hack. Find a better wat to eliminate
+								// whitespaces, line return etc. without adding empty strings
+								// to the words_arr. However, only if there's speed or memory gains.
+								var words = item_info.replace(/[^\wåäöÅÄÖ\s]/g, '');
+								words = words.replace(/[\s]/g, ',')
+								var words_arr = words.split(',');
+								console.log('Array with words: '+words_arr);
+								
+								for (x in words_arr) {
+									// How important words of char length < 3 can there be in the
+									// dictionary of humanity?
+									if (words_arr[x].length > 2) {
+										console.log('Adding word to dictionary: '+words_arr[x]);
+										redis.sadd('d:'+words_arr[x].toLowerCase(), item_id);
+									}
+								}
+
+								// Add email and remove TTL
+								redis.hset('i:'+item_id, 'email', session_data.uploader_email);
+
+								// Add item to user set (also in dictionary)
+								redis.sadd('d:'+session_data.uploader_email, item_id);
+
+								output.items.push({
+									key: item_id,
+									info: item_info.substring(0, 70)
+								});
+								output.count++;
+								
+								renderHtml(res, 'approved.html', output);
+							});
+						});
+					});
+				} else {
+					res.writeHead(404);
+					res.end();
+				}
+			});
+		} else {
+			res.writeHead(404);					
+			res.end();
+		}
+	}
 }
 
 function getQueryString(qstr) {
